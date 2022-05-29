39b893ff13fe79021a6039877cdb27f771647c33
foysalimran <fimran2011@gmail.com>
2022-05-29 07:00:03 +0600

    fixings

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,21 @@ $(document).ready(function () {
 		$(".pricing__value").toggleClass('pricing__value--show pricing__value--hide');	
 	});
 });
+
+
+$('#nav').onePageNav({
+currentClass: 'current',
+changeHash: false,
+scrollSpeed: 50,
+filter: '',
+easing: 'swing',
+begin: function() {
+	//I get fired when the animation is starting
+},
+end: function() {
+	//I get fired when the animation is ending
+},
+scrollChange: function($currentListItem) {
+	//I get fired when you enter a section and I pass the list item of the section
+}
+});
