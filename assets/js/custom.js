5de26d23d27447dd682b633e37b25d9eca80ccd1
sakhawat9 <sakhawathossain7969@gmail.com>
2022-06-01 10:52:01 +0600

    add ajax

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -230,6 +230,57 @@
     });
   };
 
+  	/******************** 17. AJAX MAILCHIMP SUBSCRIBE ********************/
+	PATH.ajaxChimp = function () {
+		$("#subscribe-mailchimp").ajaxChimp({
+			callback: mailchimpCallback,
+			url: "http:////unitetheme.us12.list-manage.com/subscribe/post?u=5e0141c017272ff01c6c3a091&amp;id=335f7c3601" // Replace your mailchimp post url inside double quote "".  
+		});
+
+		function mailchimpCallback(resp) {
+		   var error_msg = $('#subscribe-mailchimp').find('.error-msg');
+		   var success_msg = $('#subscribe-mailchimp').find('.success-msg');
+		   
+		   if(resp.result === 'success') {
+			  error_msg.fadeOut(200);
+			  success_msg.html(resp.msg).fadeIn(200);
+		   } else if(resp.result === 'error') {
+			  success_msg.fadeOut(200);
+			  error_msg.html(resp.msg).fadeIn(200);
+		   }  
+		};
+	}
+  /******************** 18. LOCAL SUBSCRIPTION ********************/
+	PATH.localSubs = function () {
+		$("#subscribe").submit(function (e) {
+			e.preventDefault();
+			var email = $("#subscriber-email").val();
+			var dataString = 'email=' + email;
+
+			function isValidEmail(emailAddress) {
+				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+				return pattern.test(emailAddress);
+			};
+
+			if (isValidEmail(email)) {
+				$.ajax({
+					type: "POST",
+					url: "assets/subscribe/subscribe.php",
+					data: dataString,
+					success: function () {
+						$('.success-msg').fadeIn(1000);
+						$('.error-msg').fadeOut(500);
+						$('.hide-after').fadeOut(500);
+					}
+				});
+			} else {
+				$('.error-msg').fadeIn(1000);
+			}
+
+			return false;
+		});
+	}
+
   /******************** DOCUMENT READY FUNCTION ********************/
   $(function () {
     PATH.MenuClose();
@@ -240,6 +291,8 @@
     PATH.HowWorksSlide();
     PATH.TestimonialSlide();
     PATH.CounterUp();
+    PATH.ajaxChimp();
+    PATH.localSubs();
   });
 
   /******************** WINDOW ON SCROLL FUNCTION ********************/
